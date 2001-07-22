bca5f13efdf1c2ec5ce904389bafdbcf147fe37e
jg253 <jg253>
2001-07-22 18:07:40 +0000

    added this file to get stuff to compile, it currently does nothing and we probably need to replace it w/ the real implementation.

---
+++ TargetWJ.java
@@ -0,0 +1,13 @@
+package psl.worklets;
+
+/**
+ * place-holder
+ */
+public class TargetWJ extends WorkletJunction{
+
+    /** 
+     * just for compiling purposes
+     */
+    protected void execute() {
+    }
+}
