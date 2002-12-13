bdff555073bdb0f5c80bee2b458998b695096fba
aib7 <aib7>
2002-12-13 02:52:41 +0000

    helper class to return a pair of string from the getBytes method

---
+++ http/StringPair.java
@@ -0,0 +1,11 @@
+package psl.worklets.http;
+
+public class StringPair{
+    String string1 = null;
+    String string2 = null;
+
+    public StringPair(String s1, String s2){
+	string1 = s1;
+	string2 = s2;
+    }
+}
